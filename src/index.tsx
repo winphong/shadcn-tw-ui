4172fdd379fb45c0945030c0e57e104cad015587
winphong <winphong1011@gmail.com>
2024-05-07 20:39:12 +0800

    build: setup base config and add basic components

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,8 @@
-export const sum = (num1: number,num2: number)=> {
-    return num1+ num2
-}
+import './index.css'
 
+export * from '@/components/ui/button'
+export * from '@/components/ui/card'
+export * from '@/components/ui/dialog'
+export * from '@/components/ui/navigation-menu'
+export * from '@/components/ui/scroll-area';
+export * from '@/components/ui/separator'
