3a06fa1592d48512583c70eab35b84219d9f6079
winphong <winphong1011@gmail.com>
2024-05-07 13:34:14 +0800

    build: setup basic build config

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+export const sum = (num1: number,num2: number)=> {
+    return num1+ num2
+}
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      name: "@shad-tw/ui", // Name of your library
+      entry: "src/index.tsx", // Entry point for your library (optional)
+      fileName: (format) => `@shad-tw-ui/main.${format}.js`, // Output filename format
+    },
+  },
+});
